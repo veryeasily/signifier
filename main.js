ac0db5f07eede7df9176d8d88717be8406293dc4
Luke Underwood <lunde@math.ku.edu>
2012-04-28 02:52:51 -0500

    modified:   images/icon.png 	modified:   images/icon2.png 	deleted:    inject/.text.coffee.swp 	deleted:    inject/links.coffee 	deleted:    inject/links.js 	modified:   main.js 	modified:   manifest.json

--- main.js
+++ main.js
@@ -1,23 +1,10 @@
-/*
-	var grapheme_on = false;
+(function() {
+  var grapheme_on;
 
-	chrome.browserAction.onClicked.addListener(function(tab) {
-		if (grapheme_on == false)
-		{
-			console.log("grapheme was false");
-			chrome.tabs.executeScript(null, {file:"scripts/jquery.min.js"}, function() {
-				chrome.tabs.executeScript(null, {file:"scripts/jquery.couch.js"}, function(){
-					chrome.tabs.executeScript(null, {file:"inject/grapheme.js"}, function() {
-						grapheme_on = true;
-					});
-				});
-			});
-		}
-		else
-		{
-			console.log("grapheme was already on?");
-			chrome.tabs.sendRequest(tab.id, {'closeGrapheme': true}, function(response) { });
-			grapheme_on = false;
-		}
-	});
-*/
+  grapheme_on = false;
+
+  chrome.browserAction.onClicked.addListener(function(tab) {
+    return chrome.tabs.sendRequest(tab.id, true, function(response) {});
+  });
+
+}).call(this);
