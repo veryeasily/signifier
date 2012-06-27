a71a1eaeecf043a9f8d199765840086f69f41d6d
Luke Underwood <lunde@math.ku.edu>
2012-06-26 22:33:12 -0500

    new file:   mixins.styl 	modified:   popup.coffee 	modified:   popup.css 	modified:   popup.html 	modified:   popup.js 	new file:   positions.styl

--- chrome/popup.coffee
+++ chrome/popup.coffee
@@ -1,20 +1,15 @@
-$("#signer").css
-	"background-color": "red"
-	top: "5px"
-	left: "5px"
+#  Wait for document to load by wrapping in jQuery
+$ ->
+	$("#signer").addClass 'addButton'
+	$("#signer2").addClass 'remButton'
 
-$("#signer2").css
-	"background-color": "blue"
-	top: "100px"
-	left: "100px"
+	makeIt = (e) ->
+		chrome.extension.sendRequest {greeting: "makeSign"}, (res) ->
+			console.log res
 
-makeIt = (e) ->
-	chrome.extension.sendRequest {greeting: "makeSign"}, (res) ->
-		console.log res
+	removeIt = (e) ->
+		chrome.extension.sendRequest {greeting: "removeSign"}, (res) ->
+			console.log res
 
-removeIt = (e) ->
-	chrome.extension.sendRequest {greeting: "removeSign"}, (res) ->
-		console.log res
-
-$("#signer").on "click", makeIt
-$("#signer2").on "click", removeIt
+	$("#signer").on "click", makeIt
+	$("#signer2").on "click", removeIt
--- chrome/popup.js
+++ chrome/popup.js
@@ -1,33 +1,22 @@
-var makeIt, removeIt;
 
-$("#signer").css({
-  "background-color": "red",
-  top: "5px",
-  left: "5px"
+$(function() {
+  var makeIt, removeIt;
+  $("#signer").addClass('addButton');
+  $("#signer2").addClass('remButton');
+  makeIt = function(e) {
+    return chrome.extension.sendRequest({
+      greeting: "makeSign"
+    }, function(res) {
+      return console.log(res);
+    });
+  };
+  removeIt = function(e) {
+    return chrome.extension.sendRequest({
+      greeting: "removeSign"
+    }, function(res) {
+      return console.log(res);
+    });
+  };
+  $("#signer").on("click", makeIt);
+  return $("#signer2").on("click", removeIt);
 });
-
-$("#signer2").css({
-  "background-color": "blue",
-  top: "100px",
-  left: "100px"
-});
-
-makeIt = function(e) {
-  return chrome.extension.sendRequest({
-    greeting: "makeSign"
-  }, function(res) {
-    return console.log(res);
-  });
-};
-
-removeIt = function(e) {
-  return chrome.extension.sendRequest({
-    greeting: "removeSign"
-  }, function(res) {
-    return console.log(res);
-  });
-};
-
-$("#signer").on("click", makeIt);
-
-$("#signer2").on("click", removeIt);
